b60b66638f52f3ea18c515d1236f13d1157884b5
jake-bestland <beja0405@gmail.com>
2025-01-13 15:02:47 -0600

    Updating Checklist Model

--- checklist/models.py
+++ checklist/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Checklist(models.Model):
-    """Model representing an item on a checklist."""
+    """Model representing a checklist."""
+    title = models.CharField(max_length=100, default='New List')
+    category = models.CharField(max_length=100, null=True, blank=True)
     entry = models.TextField()
+    due_date = models.DateField(null=True, blank=True)
+
+
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
