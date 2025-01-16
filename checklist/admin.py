f6938ff61326c115e308a4a9b1daf1e0d5ac0f00
jake-bestland <beja0405@gmail.com>
2025-01-16 17:35:32 -0600

    Updating Models and homepage template

--- checklist/admin.py
+++ checklist/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Checklist
+from .models import Checklist, Entry
 
 # Register your models here.
 admin.site.register(Checklist)
+admin.site.register(Entry)
--- checklist/models.py
+++ checklist/models.py
@@ -1,11 +1,48 @@
+from django.utils import timezone # (to add date for when to 'complete' checklist, e.g. when to go shopping)
 from django.db import models
+from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+from django.conf import settings
+import uuid
 
 # Create your models here.
+
+# def shopping_day():
+#     return  *find days from sunday* eg. one week from now is: timezone.now() + timezone.timedelta(days=7) 
+
+### future - could create different types of lists instead of one checklist.  eg. grocery list, to-do, gift ideas, etc
+
+class Checklist(models.Model):  
+    """Model representing a checklist."""
+    title = models.CharField(max_length=100, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+
+
+    def get_absolute_url(self):
+        return reverse("checklist-detail", args=[self.id])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+    
+
 class Entry(models.Model):
     """Model representing an entry for a checklist."""
-    name = models.CharField(max_length=200, unique=True, help_text="Add and entry to your list.")
+
+    name = models.CharField(max_length=200, unique=True, help_text="Add an entry to your list.")
+    notes = models.TextField(null=True, blank=True)  # maybe change to CharField
+    # due_date = models.DateTimeField(null=True, blank=True)
+    # sub_category = models.CharField(max_length=100, null=True, blank=True)
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE, null=True)      ### maybe change to ManytoManyField later?
+    ## quantity
+
+    def get_absolute_url(self):
+        return reverse(
+            "entry-update", args=[str(self.checklist.id), str(self.id)]
+        )
 
     def __str__(self):
         """String for representing the Model object."""
@@ -19,17 +56,3 @@ class Entry(models.Model):
                 violation_error_message = "Entry already exists (case insensitive match)"
             ),
         ]
-
-
-class Checklist(models.Model):
-    """Model representing a checklist."""
-    title = models.CharField(max_length=100, default='New List')
-    category = models.CharField(max_length=100, null=True, blank=True)
-    entry = models.ManyToManyField(Entry, help_text="Add an entry to your list.")
-    due_date = models.DateField(null=True, blank=True)
-
-
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.title
