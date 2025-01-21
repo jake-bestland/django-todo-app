58f0c0df1000dbeceaff9d1bd175235eb5c0c93b
jake-bestland <beja0405@gmail.com>
2025-01-20 18:03:22 -0600

    updating urls.py

--- checklist/urls.py
+++ checklist/urls.py
@@ -6,9 +6,13 @@ app_name = 'checklist'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('<str:username>/', views.UserChecklistListView.as_view(), name='my-lists'),
-    path('<str:username>/<slug:slug>/', views.ChecklistDetailView.as_view(), name="checklist-detail"),
-    path('<str:username>/add/', views.ChecklistCreate.as_view(), name="checklist-add"),
-    path('<str:username>/<slug:slug>/entry/add', views.EntryCreate.as_view(), name="entry-add"),
-    path('<str:username>/<slug:slug>/entry/<int:pk>/', views.EntryUpdate.as_view(), name="entry-update"),
+    path('<str:username>/', views.UserChecklistListView.as_view(), name='index'),
+    path('<str:username>/list/<int:list_id>/', views.EntryListView.as_view(), name="list"),
+
+    path('<str:username>/list/add/', views.ChecklistCreate.as_view(), name="list-add"),
+    path('<str:username>/list/<int:pk>/delete//', views.ChecklistDelete.as_view(), name="list-delete"),
+
+    path('<str:username>/<int:list_id>/entry/add', views.EntryCreate.as_view(), name="entry-add"),
+    path('<str:username>/<int:list_id>/entry/<int:pk>/', views.EntryUpdate.as_view(), name="entry-update"),
+    path('<str:username>/list/<int:list_id>/entry/<int:pk>/delete/', views.EntryDelete.as_view(), name="entry-delete"),
 ]
