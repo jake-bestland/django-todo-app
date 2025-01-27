ad2d321bd0c94a947ccd97753b67dfb506c73917
jake-bestland <beja0405@gmail.com>
2025-01-27 16:16:48 -0600

    Adding to Profile class and updating Checklist and Entry

--- checklist/models.py
+++ checklist/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 
 # Create your models here.
@@ -15,15 +18,30 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(
+        "self",
+        related_name="friends_with",
+        symmetrical=False,
+        blank=True
+    )
 
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            user_profile = Profile(user=instance)
+            user_profile.save()
+            user_profile.friends.add(instance.profile)
+            user_profile.save()
+
+    def __str__(self):
+        return self.user.username
 
 
 
 class Checklist(models.Model):  
     """Model representing a checklist."""
     title = models.CharField(max_length=100, unique=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(Profile, on_delete=models.RESTRICT, null=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
 
     def save(self, *args, **kwargs):
@@ -32,8 +50,8 @@ class Checklist(models.Model):
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
 
-    def get_absolute_url(self):
-        return reverse("list", args=[str(self.slug)])
+    # def get_absolute_url(self):
+    #     return reverse("list", args=[str(self.slug)])
 
     def __str__(self):
         """String for representing the Model object."""
@@ -44,9 +62,12 @@ class Entry(models.Model):
     """Model representing an entry for a checklist."""
 
     name = models.CharField(max_length=100, help_text="Add an entry to your list.")
-    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
-    notes = models.TextField(null=True, blank=True)  # maybe change to CharField # or change to quantity
-    # due_date = models.DateTimeField(null=True, blank=True)
+    checklist = models.ForeignKey(Checklist, on_delete=models.RESTRICT, null=True)
+    completed = models.BooleanField(default=False)
+
+    creation_date = models.DateTimeField('date created', auto_now_add=True, blank=True, null=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    # notes = models.TextField(null=True, blank=True)  # maybe change to CharField # or change to quantity
     # sub_category = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
