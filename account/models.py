c74af1e66fd54f48c7cfb57d40371f6f07063c30
jake-bestland <beja0405@gmail.com>
2025-02-20 09:18:21 -0600

    fixed create_superuser method

--- account/models.py
+++ account/models.py
@@ -31,7 +31,7 @@ class MyAccountManager(BaseUserManager):
         return user
     
     def create_superuser(self, email, first_name, last_name, username, password):
-        user = self.create_superuser(
+        user = self.create_user(
             email=self.normalize_email(email),
             first_name=first_name,
             last_name=last_name,
